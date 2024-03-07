99c73f4bb2339d18b338daf14d53e359d5c99681
aslah <aslahcholasseri@gmail.com>
2024-03-07 18:15:54 -0500

    completed the ui desing

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+import './Banner.css'
+
+function Banner() {
+
+
+
+  return (
+    <div className='banner'>
+      <div className='content'>
+        <h1 className='title'>movie name</h1>
+        <div className='banner-button'>
+          <button className='button'>play</button>
+          <button className='button'>  My List  </button>
+
+
+
+        </div>
+        <h1 className='description'>hellofsjskjslakjafjfjsfjskj
+          skjfskjsjsfskjfskjfskjfs</h1>
+
+
+      </div>
+
+      <div className="fade_bottom">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Banner
