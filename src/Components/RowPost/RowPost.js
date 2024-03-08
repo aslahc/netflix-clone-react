abf5801879007e98262176a93e409729c3d3793c
aslah <aslahcholasseri@gmail.com>
2024-03-08 10:16:14 -0500

    completed the dynamic ui

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,32 +1,67 @@
 import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube'
 import './RowPost.css'
 import axios from '../../axios'
-import { baseUrl, APi_key, imageUrl } from '../../constants/constants'
-function RowPost() {
+import {  imageUrl ,APi_key} from '../../constants/constants'
 
-  const [movies, setMovies] = useState([])
+function RowPost(props) {
+
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState('');
+
   useEffect(() => {
-    axios.get(`${baseUrl}/discover/tv?api_key=${APi_key}&with_networks=213`).then(response => {
-      console.log("this is secend data", response.data)
-      setMovies(response.data.results)
-    }).catch(err => {
-      alert('Network error')
-    })
-  }, [])
+    axios.get(props.url)
+      .then(response => {
+        console.log("This is second data", response.data);
+        setMovies(response.data.results);
+      })
+      .catch(err => {
+        alert('Network error');
+      });
+  }, [props.url]); // added props.url to dependency array
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+    console.log(id);
+    axios.get(`/movie/${id}/videos?api_key=${APi_key}&language=en-US`)
+      .then(response => {
+        console.log(response.data);
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0].key); // corrected accessing key
+        } else {
+          console.log("Trailer not available");
+        }
+      })
+      .catch(err => {
+        console.log("Error fetching trailer:", err);
+      });
+  };
+
   return (
-
-
     <div className='row'>
-      <h2>Netflix Orginals</h2>
+      <h2>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj) =>
-          <img className='poster' src={`${imageUrl + obj.backdrop_path}`} alt='poster'></img>
-
+          <img
+            key={obj.id} // added key prop for unique identifier
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? 'smallPoster' : 'poster'}
+            src={`${imageUrl}${obj.backdrop_path}`} // corrected image URL concatenation
+            alt='poster'
+          />
         )}
-
       </div>
+      {urlId && <YouTube opts={opts} videoId={urlId} />} {/* Render YouTube component if urlId exists */}
     </div>
-  )
+  );
 }
+
 
 export default RowPost
---
+++ src/urls.js
@@ -0,0 +1,5 @@
+import { baseUrl, APi_key,  } from './constants/constants'
+
+
+export const originals = `${baseUrl}/discover/tv?api_key=${APi_key}&with_networks=213`;
+export const action = `${baseUrl}/discover/movie?api_key=${APi_key}&with_genres=28`;
